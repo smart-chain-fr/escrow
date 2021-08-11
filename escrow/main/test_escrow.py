e77af12fed3bf34ceb2bcbb8d8be906023b3a1e8
Lucas <lucaslevy10@gmail.com>
2021-08-11 17:30:39 +0200

    feat(test): initialize_escrow tests

--- escrow/main/test_escrow.py
+++ escrow/main/test_escrow.py
@@ -2,12 +2,19 @@ from unittest import TestCase
 from contextlib import contextmanager
 from copy import deepcopy
 from pytezos import ContractInterface, pytezos, MichelsonRuntimeError
+from hashlib import blake2b
 
 alice = 'tz1hNVs94TTjZh6BZ1PM5HL83A7aiZXkQ8ur'
 admin = 'tz1fABJ97CJMSP2DKrQx2HAFazh6GgahQ7ZK'
 bob = 'tz1c6PPijJnZYjKiSQND4pMtGMg6csGeAiiF'
 initial_storage = ContractInterface.from_file("Escrow.tz").storage.dummy()
 initial_storage["admin"] = admin
+empty_comment = {
+    "buyer": "",
+    "seller": ""
+}
+state = "Initialized!"
+
 
 class EscrowContractTest(TestCase):
 
@@ -34,3 +41,44 @@ class EscrowContractTest(TestCase):
         self.assertEqual([], res.operations)
         with self.raisesMichelsonError("Only the admin can run this function"):
             self.escrow.setAdmin(bob).interpret(storage=init_storage, source=alice)
+
+    def test_initialize_escrow(self):
+        init_storage = deepcopy(initial_storage)
+        res = self.escrow.initialize_escrow({
+            "seller": bob,
+            "broker": None,
+            "product": "NFT de Charles",
+            "price": 1000,
+            "id": blake2b("NFT de Charles".encode()).digest()
+        }).interpret(storage=init_storage, sender=alice, now=420, amount=1000)
+        expected_escrows = {
+            blake2b("NFT de Charles".encode()).digest():
+                {
+                    "buyer": alice,
+                    "seller": bob,
+                    "broker": None,
+                    "product": "NFT de Charles",
+                    "price": 1000,
+                    "comment": empty_comment,
+                    "state": state,
+                    "time": 420
+                }
+        }
+        self.assertDictEqual(res.storage["escrows"], expected_escrows)
+        self.assertEqual(res.operations, [])
+        with self.raisesMichelsonError("Not enough XTZ to initialize escrow"):
+            self.escrow.initialize_escrow({
+                "seller": bob,
+                "broker": None,
+                "product": "NFT de Charles",
+                "price": 1001,
+                "id": blake2b("NFT de Charles".encode()).digest()
+            }).interpret(storage=init_storage, sender=alice, now=420, amount=1000)
+        with self.raisesMichelsonError("Not enough XTZ to initialize escrow"):
+            self.escrow.initialize_escrow({
+                "seller": bob,
+                "broker": None,
+                "product": "NFT de Charles",
+                "price": 999,
+                "id": blake2b("NFT de Charles".encode()).digest()
+            }).interpret(storage=init_storage, sender=alice, now=420, amount=1000)
