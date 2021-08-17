26c9044952b6bb73c790590b485247873c207e35
HelderSalvador <helder.salvador@smart-chain.fr>
2021-08-17 17:45:28 +0200

    feat(contract + test) : Update addComment function and add its test

--- escrow/main/test_escrow.py
+++ escrow/main/test_escrow.py
@@ -8,12 +8,12 @@ alice = 'tz1hNVs94TTjZh6BZ1PM5HL83A7aiZXkQ8ur'
 admin = 'tz1fABJ97CJMSP2DKrQx2HAFazh6GgahQ7ZK'
 bob = 'tz1c6PPijJnZYjKiSQND4pMtGMg6csGeAiiF'
 oscar = 'tz1Phy92c2n817D17dUGzxNgw1qCkNSTWZY2'
+fox = 'tz1XH5UyhRCUmCdUUbqD4tZaaqRTgGaFXt7q'
 initial_storage = ContractInterface.from_file("Escrow.tz").storage.dummy()
 initial_storage["admin"] = admin
-empty_comment = {
-    "buyer": "",
-    "seller": ""
-}
+
+empty_comment = {}
+
 
 only_admin = "Only admin"
 access_denied = "Access denied"
@@ -33,7 +33,9 @@ state_seller_canceled = "Seller canceled"
 state_canceled = "Canceled"
 state_validated = "Validated"
 escrow_key = blake2b("NFT de Charles".encode()).digest()
-
+not_existing_escrow_key = blake2b("NFTTTTT de Charles".encode()).digest()
+escrow_comment = {"id" : 1, "idEscrow" : escrow_key, "message" : "I'm an important message"}
+not_working_escrow_comment = {"id" : 1, "idEscrow" : not_existing_escrow_key, "message" : "I'm an important message"}
 
 class EscrowContractTest(TestCase):
 
@@ -197,15 +199,13 @@ class EscrowContractTest(TestCase):
         # Random user tries to validate an escrow for someone else (fails) #
         ####################################################################
         with self.raisesMichelsonError(access_denied):
-            self.escrow.agree(escrow_key).interpret(storage=init_storage,
-                                                                                     sender=oscar, now=500)
+            self.escrow.agree(escrow_key).interpret(storage=init_storage,sender=oscar, now=500)
 
         ###############################################################
         # Seller tries to validate an escrow for someone else (fails) #
         ###############################################################
         with self.raisesMichelsonError(access_denied):
-            self.escrow.agree(escrow_key).interpret(storage=init_storage,
-                                                                                     sender=bob, now=500)
+            self.escrow.agree(escrow_key).interpret(storage=init_storage, sender=bob, now=500)
         ###############################################################
         # User tries to validate an escrow that doesn't exist (fails) #
         ###############################################################
@@ -324,3 +324,64 @@ class EscrowContractTest(TestCase):
         ###############################################################
         with self.raisesMichelsonError(access_denied):
             self.escrow.cancel_escrow(escrow_key).interpret(storage=init_storage, sender=admin)
+        
+    def test_add_comment(self):
+        init_storage = deepcopy(initial_storage)
+
+        #####################################################################################
+        #                      Initializes an escrow in the storage                         #
+        #####################################################################################
+        expected_escrows = {
+                escrow_key:
+                    {
+                        "buyer": alice,
+                        "seller": bob,
+                        "broker": fox,
+                        "product": "NFT de Charles",
+                        "price": 1000,
+                        "comment": empty_comment,
+                        "state": state_initialized,
+                        "time": None,
+                        "proof": None
+                    }
+            }
+
+        init_storage["escrows"] = expected_escrows
+
+        #####################################################################################
+        #                        Simulates the addComment function                          #
+        #####################################################################################
+
+        comment_broker = self.escrow.addComment(escrow_comment).interpret(storage=init_storage, sender=fox, now=200)
+        comment_seller = self.escrow.addComment(escrow_comment).interpret(storage=init_storage, sender=bob, now=200)
+        comment_buyer = self.escrow.addComment(escrow_comment).interpret(storage=init_storage, sender=alice, now=200)
+        comment_admin = self.escrow.addComment(escrow_comment).interpret(storage=init_storage, sender=admin, now=200)
+        
+        print(comment_broker.storage["escrows"][escrow_key]["comment"])
+        print(comment_seller.storage["escrows"][escrow_key]["comment"])
+        print(comment_buyer.storage["escrows"][escrow_key]["comment"])
+        print(comment_admin.storage["escrows"][escrow_key]["comment"])
+
+        ##########################################################################################################
+        #       Check equality between storage and escrow comment, the tuple second key is defined by :          #
+        #               // 0 = Smartlink(admin) ; 1 = Buyer ; 2 = Seller ; 3 = Broker                            # 
+        ##########################################################################################################
+
+        self.assertEqual(comment_broker.storage["escrows"][escrow_key]["comment"], {(200,3): escrow_comment["message"]}) 
+        self.assertEqual(comment_seller.storage["escrows"][escrow_key]["comment"], {(200,2): escrow_comment["message"]}) 
+        self.assertEqual(comment_buyer.storage["escrows"][escrow_key]["comment"], {(200,1): escrow_comment["message"]}) 
+        self.assertEqual(comment_admin.storage["escrows"][escrow_key]["comment"], {(200,0): escrow_comment["message"]}) 
+
+        ###############################################################################
+        # Random user tries to add a comment to an escrow he doesn't belong in(fails) #
+        ###############################################################################
+        with self.raisesMichelsonError(access_denied):
+            self.escrow.addComment(escrow_comment).interpret(storage=init_storage, sender=oscar, now=500)
+
+        #################################################################################
+        # Random user tries to add a comment to an escrow that doesn't exists in(fails) #
+        #################################################################################
+        with self.raisesMichelsonError(doesnt_exist):
+            self.escrow.addComment(not_working_escrow_comment).interpret(storage=init_storage, sender=alice, now=500)
+
+       
